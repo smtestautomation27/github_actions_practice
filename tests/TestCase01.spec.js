8c31c605c82229689aedbc34f216bdf92a4c7226
suman2709 <sumanmallick27@gmail.com>
2026-06-26 14:54:59 +0530

    Minor Fixes

--- tests/TestCase01.spec.js
+++ tests/TestCase01.spec.js
@@ -1,7 +1,7 @@
 import {test, expect, chromium} from '@playwright/test';
 
 test.describe('Test Case 01', () => {
-    test('Navigate to the page and check the title', async ({ page }) => {
+    test.beforeEach('Navigate to the page and check the title', async ({ page }) => {
         await page.goto('https://www.saucedemo.com/');
         await expect(page).toHaveTitle('Swag Labs');
     });
@@ -14,6 +14,10 @@ test.describe('Test Case 01', () => {
     });
 
     test('Check the logout functionality', async ({ page }) => {
+        await page.getByPlaceholder('Username').fill('standard_user');
+        await page.getByPlaceholder('Password').fill('secret_sauce');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
         await page.locator('#react-burger-menu-btn').click();
         await page.getByRole('link', { name: 'Logout' }).click();
         await expect(page).toHaveURL('https://www.saucedemo.com/');
