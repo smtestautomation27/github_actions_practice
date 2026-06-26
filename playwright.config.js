c3ce2077336317623c1b3fd1b4db1af94ebdbb67
suman2709 <sumanmallick27@gmail.com>
2026-06-26 14:46:50 +0530

    Minor Changes

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     /* trace: 'on-first-retry', */
-    headless: false,
+    headless: process.env.CI ? true : false,
     viewport: { width: 1280, height: 720 },
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
