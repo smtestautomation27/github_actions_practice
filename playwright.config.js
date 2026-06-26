61102570639112362b104da298f4a3acb4595438
suman2709 <sumanmallick27@gmail.com>
2026-06-26 14:17:21 +0530

    Minor Changes

--- playwright.config.js
+++ playwright.config.js
@@ -23,14 +23,21 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /* reporter: 'html', */
+  reporter: [['html'], ['github'], ['dot']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /* trace: 'on-first-retry', */
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+
   },
 
   /* Configure projects for major browsers */
@@ -40,7 +47,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /* {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -48,7 +55,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    }, */
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/TestCase01.spec.js
@@ -0,0 +1,21 @@
+import {test, expect, chromium} from '@playwright/test';
+
+test.describe('Test Case 01', () => {
+    test('Navigate to the page and check the title', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        await expect(page).toHaveTitle('Swag Labs');
+    });
+
+    test('Check the login functionality', async ({ page }) => {
+        await page.getByPlaceholder('Username').fill('standard_user');
+        await page.getByPlaceholder('Password').fill('secret_sauce');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    });
+
+    test('Check the logout functionality', async ({ page }) => {
+        await page.locator('#react-burger-menu-btn').click();
+        await page.getByRole('link', { name: 'Logout' }).click();
+        await expect(page).toHaveURL('https://www.saucedemo.com/');
+    });
+})
